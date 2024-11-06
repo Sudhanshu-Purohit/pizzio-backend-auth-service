e7d05c2a4a26ac3b539476dd81e7eb5ba90a8f9b
Sudhanshu Purohit <purohitsudhanshu294@gmail.com>
2024-11-07 01:07:41 +0530

    add error handler

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,27 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import createHttpError, { HttpError } from "http-errors";
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+app.get("/", (req, res, next) => {
+    const err = createHttpError(401, "You can't access this route");
+    next(err);
+});
+
+// global error handler middleware - it should be in the last of file
+app.use((err: HttpError, req: Request, res: Response) => {
+    const statusCode = err.statusCode || 500;
+
+    res.status(statusCode).json({
+        errors: [
+            {
+                type: err.name,
+                msg: err.message,
+                path: "",
+                location: "",
+            },
+        ],
+    });
 });
 
 export default app;
